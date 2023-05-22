bccbc85aceab9f39b4d4849b60e1f54bca54ef4a
your_username <stztrujillo@hotmail.com>
2023-05-22 12:41:03 -0500

    implemented buy kitOne and buy kitTwo

--- contracts/Presale1.sol
+++ contracts/Presale1.sol
@@ -11,13 +11,26 @@ contract Presale1 {
   address constant private PANCAKESWAP_ROUTER_V3 = 0x9a489505a00cE272eAa5e07Dba6491314CaE3796; //TESTNET
   uint256 constant public DECIMALS = 18;
 
-  //~~~~~ State variables ~~~~~
-  uint256 public immutable PRICE;
-  address[] public buyers;
-  mapping(address => bool) hasPurchased;
+  //~~~~~ Enum ~~~~~
+  enum Kits {Kit1, Kit2}
 
-  constructor(uint256 _price){
-    PRICE = _price * 10**DECIMALS;
+  //~~~~~ Immutable variables ~~~~~
+  uint256 public immutable PRICE_KIT_ONE;
+  uint256 public immutable PRICE_KIT_TWO;
+  uint256 public immutable MAX_SUPPLY_KIT_ONE;
+  uint256 public immutable MAX_SUPPLY_KIT_TWO;
+
+  //~~~~~ State variables ~~~~~
+  address[] public buyersKitOne;
+  address[] public buyersKitTwo;
+  mapping(address => bool) hasPurchasedKitOne;
+  mapping(address => bool) hasPurchasedKitTwo;
+
+  constructor(uint256 _price1, uint256 _price2, uint256 _maxSupply1, uint256 _maxSupply2){
+    PRICE_KIT_ONE = _price1 * 10**DECIMALS;
+    PRICE_KIT_TWO = _price2 * 10**DECIMALS;
+    MAX_SUPPLY_KIT_ONE = _maxSupply1;
+    MAX_SUPPLY_KIT_TWO = _maxSupply2;
   }
 
   /**
@@ -30,6 +43,69 @@ contract Presale1 {
     _;
   }
 
+  /**
+   * 
+   * @dev users can buy the pre sale by sending USDT
+   * 
+   * NOTE: msg.sender must approve 'PRICE' quantity of tokens first
+   * 
+   * Requirements:
+   * - msg.sender must be an EOA (wallet).
+   * - the user can purchase only x1 kitOne per wallet.
+   * - user must approve "PRICE_KIT_ONE" quantity of USDT for this contract
+   * 
+   */
+  function buyKitOne() external onlyWallet() {
+    address _sender = msg.sender;
+    require(!hasPurchasedKitOne[_sender], "Whitelist: You have already purchased kit one.");
+    require(IERC20(USDT).transferFrom(_sender,address(this),PRICE_KIT_ONE));
+    buyersKitOne.push(_sender);
+    hasPurchasedKitOne[_sender] = true;
+  }
+
+  /**
+   * 
+   * @dev users can buy the pre sale by sending USDT
+   * 
+   * NOTE: msg.sender must approve 'PRICE_KIT_TWO' quantity of tokens first
+   * 
+   * Requirements:
+   * - msg.sender must be an EOA (wallet).
+   * - the user can purchase only x1 kitTwo per wallet.
+   * - user must approve "PRICE_KIT_TWO" quantity of USDT for this contract
+   * 
+   */
+  function buyKitTwo() external onlyWallet() {
+    address _sender = msg.sender;
+    require(!hasPurchasedKitTwo[_sender], "Whitelist: You have already purchased kit one.");
+    require(IERC20(USDT).transferFrom(_sender,address(this),PRICE_KIT_TWO));
+    buyersKitTwo.push(_sender);
+    hasPurchasedKitTwo[_sender] = true;
+  }
+
+  /**
+   * 
+   * @dev returns true if the user has purchased kitOne
+   * 
+   * @param _user address of the user to verify if has purchased
+   *
+   */
+  function userHasPurchasedKitOne(address _user) external view returns(bool){
+    return hasPurchasedKitOne[_user];
+  }
+
+  /**
+   * 
+   * @dev returns true if the user has purchased kitTwo
+   * 
+   * @param _user address of the user to verify if has purchased
+   *
+   */
+  function userHasPurchasedKitTwo(address _user) external view returns(bool){
+    return hasPurchasedKitTwo[_user];
+  }
+
+
   //TO DO
   function _createPair() internal returns(address pair) {
 
@@ -39,38 +115,6 @@ contract Presale1 {
   function _addLiquidity() internal{
 
   }
-
-  /**
-   * 
-   * @dev users can buy the pre sale by sending USDT
-   * 
-   * NOTE: msg.sender must approve 'PRICE' quantity of tokens first
-   * 
-   * Requirements:
-   * - msg.sender must be an EOA (wallet).
-   * - the user can purchase only 1 pre sale per wallet.
-   * - user must approve "PRICE" quantity of USDT for this contract
-   * 
-   */
-  function buyPreSale() external onlyWallet() {
-    address _sender = msg.sender;
-    require(!hasPurchased[_sender], "Whitelist: You have already purchased.");
-    require(IERC20(USDT).transferFrom(_sender,address(this),PRICE));
-    buyers.push(_sender);
-    hasPurchased[_sender] = true;
-  }
-
-  /**
-   * 
-   * @dev returns true if the user has purchased the pre sale
-   * 
-   * @param _user address of the user to verify if has purchased
-   *
-   */
-  function userHasPurchased(address _user) external view returns(bool){
-    return hasPurchased[_user];
-  }
-
     
 
 }
