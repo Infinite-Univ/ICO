00476bfafd6198936b181097270c90f5b96076e2
kypanz <kypanz95@gmail.com>
2023-07-07 05:48:36 -0300

    Creando script para hacer el deploy en bsct testnet

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,14 @@
 const hre = require("hardhat");
 
+const NAME_CONTRACT = 'XXXX'
+
 async function main() {
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy();
-
-  await lock.deployed();
-
-  console.log(`Lock deployed to ${lock.address}`);
+  const Contract = await hre.ethers.getContractFactory(NAME_CONTRACT); // Mock Presale here
+  const contract = await Contract.deploy();
+  await contract.deployed();
+  console.log(`Smart ${NAME_CONTRACT}  deployed to ${contract.address}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
