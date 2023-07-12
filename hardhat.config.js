8a2109684968de9e271e446cac3fa0c911083795
kypanz <kypanz95@gmail.com>
2023-07-12 12:28:43 -0300

    bajando la version a una estable sporotada por hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
+
+const { ACOCUNT_PRIVATE_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    bsctest: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+      accounts: [ACOCUNT_PRIVATE_KEY]
+    }
+  }
 };
