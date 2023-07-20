777e08fe231e6f96ee1545150c5dd1184db16265
kypanz <kypanz95@gmail.com>
2023-07-19 22:51:03 -0300

    adding deploy links

--- scripts/deployTokens.js
+++ scripts/deployTokens.js
@@ -2,6 +2,12 @@ const hre = require("hardhat");
 const NAME_CONTRACT = 'MockUSDT';
 const NAME_CONTRACT_TWO = 'SuperNova';
 
+/*
+    Last deploy
+    Smart contract MockUSDT  deployed to https://testnet.bscscan.com/address/0xC23a1548cf77799D0a481eeb5010f76C4D356ed9
+    Smart contract SuperNova  deployed to https://testnet.bscscan.com/address/0xd531FCb1c3B9447Ae9815A69EeB95D2c0178460E
+*/
+
 async function main() {
 
     // Deploy [ Token ] Mock USDT
